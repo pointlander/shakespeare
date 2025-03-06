b7ca6ba3e053270a3c47db62b13626da66d30292
Andrew Snodgrass <pointlander@gmail.com>
2025-03-06 17:49:22 -0600

    Added neural network

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/pointlander/shakespeare
 
 go 1.23.3
+
+require (
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/pointlander/gradient v0.0.0-20240226214843-e3d2a19564fd // indirect
+	github.com/ziutek/blas v0.0.0-20190227122918-da4ca23e90bb // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -9,10 +9,34 @@ import (
 	"embed"
 	"fmt"
 	"io"
+	"math"
+	"math/rand"
+	"sort"
+	"strings"
+
+	"github.com/pointlander/gradient/tf32"
 )
 
 //go:embed books/*
 var Data embed.FS
+
+const (
+	// B1 exponential decay of the rate for the first moment estimates
+	B1 = 0.8
+	// B2 exponential decay rate for the second-moment estimates
+	B2 = 0.89
+	// Eta is the learning rate
+	Eta = 1.0e-3
+)
+
+const (
+	// StateM is the state for the mean
+	StateM = iota
+	// StateV is the state for the variance
+	StateV
+	// StateTotal is the total number of states
+	StateTotal
+)
 
 func main() {
 	file, err := Data.Open("books/100.txt.utf-8.bz2")
@@ -25,12 +49,184 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	symbols, s := make(map[rune]int), 0
+	in, symbols, isymbols, s := make([]byte, 0, 8), make(map[rune]int), make(map[int]rune), 0
 	for _, v := range string(data) {
 		if _, has := symbols[v]; !has {
 			symbols[v] = s
+			isymbols[s] = v
 			s++
 		}
+		in = append(in, byte(symbols[v]))
 	}
 	fmt.Println(s)
+
+	in = in[:8*1024]
+	type Vector struct {
+		Vector []float32
+		Symbol byte
+	}
+	mind, index := make([]Vector, len(in)), 0
+	m := NewMixer()
+	m.Add(0)
+
+	others := tf32.NewSet()
+	others.Add("input", 8*256, len(in))
+	others.Add("output", len(symbols), len(in))
+	input, output := others.ByName["input"], others.ByName["output"]
+	for _, v := range in {
+		index = (index + 1) % len(mind)
+		vectors := m.Mix()
+		mind[index].Vector = vectors.Data
+		mind[index].Symbol = v
+		input.X = append(input.X, vectors.Data...)
+		out := make([]float32, len(symbols))
+		out[v] = 1
+		output.X = append(output.X, out...)
+		m.Add(v)
+	}
+	rng := rand.New(rand.NewSource(1))
+	set := tf32.NewSet()
+	set.Add("w", 8*256, len(symbols))
+	set.Add("b", len(symbols))
+	set.Add("w1", len(symbols), len(symbols))
+	set.Add("b1", len(symbols))
+	for i := range set.Weights {
+		w := set.Weights[i]
+		if strings.HasPrefix(w.N, "b") {
+			w.X = w.X[:cap(w.X)]
+			w.States = make([][]float32, StateTotal)
+			for i := range w.States {
+				w.States[i] = make([]float32, len(w.X))
+			}
+			continue
+		}
+		factor := math.Sqrt(2.0 / float64(w.S[0]))
+		for i := 0; i < cap(w.X); i++ {
+			w.X = append(w.X, float32(rng.NormFloat64()*factor))
+		}
+		w.States = make([][]float32, StateTotal)
+		for i := range w.States {
+			w.States[i] = make([]float32, len(w.X))
+		}
+	}
+	l := tf32.Sigmoid(tf32.Add(tf32.Mul(set.Get("w"), others.Get("input")), set.Get("b")))
+	l1 := tf32.Add(tf32.Mul(set.Get("w1"), l), set.Get("b1"))
+	loss := tf32.Avg(tf32.Quadratic(l1, others.Get("output")))
+	for i := 0; i < 256; i++ {
+		pow := func(x float32) float32 {
+			y := math.Pow(float64(x), float64(i+1))
+			if math.IsNaN(y) || math.IsInf(y, 0) {
+				return 0
+			}
+			return float32(y)
+		}
+
+		others.Zero()
+		set.Zero()
+
+		cost := tf32.Gradient(loss).X[0]
+		if math.IsNaN(float64(cost)) || math.IsInf(float64(cost), 0) {
+			break
+		}
+
+		norm := float32(0.0)
+		for _, p := range set.Weights {
+			for _, d := range p.D {
+				norm += d * d
+			}
+		}
+		norm = float32(math.Sqrt(float64(norm)))
+		b1, b2 := pow(B1), pow(B2)
+		scaling := float32(1.0)
+		if norm > 1 {
+			scaling = 1 / norm
+		}
+		for _, w := range set.Weights {
+			for l, d := range w.D {
+				g := d * scaling
+				m := B1*w.States[StateM][l] + (1-B1)*g
+				v := B2*w.States[StateV][l] + (1-B2)*g*g
+				w.States[StateM][l] = m
+				w.States[StateV][l] = v
+				mhat := m / (1 - b1)
+				vhat := v / (1 - b2)
+				if vhat < 0 {
+					vhat = 0
+				}
+				w.X[l] -= Eta * mhat / (float32(math.Sqrt(float64(vhat))) + 1e-8)
+			}
+		}
+		fmt.Println(i, cost)
+	}
+
+	{
+		m := NewMixer()
+		for _, v := range []rune("Hello world!") {
+			m.Add(byte(symbols[v]))
+		}
+		others := tf32.NewSet()
+		others.Add("input", 8*256, 1)
+		input := others.ByName["input"]
+		input.X = input.X[:cap(input.X)]
+		l = tf32.Sigmoid(tf32.Add(tf32.Mul(set.Get("w"), others.Get("input")), set.Get("b")))
+		l1 = tf32.Add(tf32.Mul(set.Get("w1"), l), set.Get("b1"))
+		type Path struct {
+			Path string
+			Cost float32
+		}
+		paths := make([]Path, 0, 8)
+		for j := 0; j < 128; j++ {
+			query := ""
+			cost := float32(0.0)
+			i := 0
+			cp := m.Copy()
+			for {
+				q := cp.Mix()
+				for i, v := range q.Data {
+					input.X[i] = v
+				}
+				max, symbol := float32(0.0), 0
+				l1(func(a *tf32.V) bool {
+					sum := float32(0.0)
+					for _, v := range a.X {
+						sum += v
+					}
+					selection, total := rng.Float32(), float32(0.0)
+					for i, v := range a.X {
+						total += v / sum
+						if selection < total {
+							cost += v / sum
+							symbol = i
+							break
+						}
+						/*if v > max {
+							max, symbol = v, is[i]
+						}*/
+					}
+					return true
+				})
+				_ = max
+				query += fmt.Sprintf("%c", isymbols[symbol])
+				cp.Add(byte(symbol))
+				i++
+				if i >= 128 && (symbol == '.' || symbol == '!' || symbol == '?') {
+					break
+				}
+				if i >= 1024 {
+					break
+				}
+			}
+			paths = append(paths, Path{
+				Path: query,
+				Cost: cost,
+			})
+		}
+		for i := range paths {
+			paths[i].Cost /= float32(len(paths[i].Path))
+		}
+		sort.Slice(paths, func(i, j int) bool {
+			return paths[i].Cost > paths[j].Cost
+		})
+		fmt.Printf(paths[0].Path)
+	}
 }
