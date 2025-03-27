df51fbad00451c6e138d4751b122fa6952397b66
Andrew Snodgrass <pointlander@gmail.com>
2025-03-27 12:51:06 -0500

    Added reinforcement learning mode

--- main.go
+++ main.go
@@ -52,6 +52,8 @@ const (
 )
 
 var (
+	// FlagRL is reinforcement learning mode
+	FlagRL = flag.Bool("rl", false, "reinforcement learning mode")
 	// FlagVDB is the vector database mode
 	FlagVDB = flag.Bool("vdb", false, "vector database mode")
 	// FlagVectors build vector db
@@ -424,6 +426,71 @@ func main() {
 		in = append(in, byte(symbols[v]))
 	}
 	fmt.Println(s)
+
+	if *FlagRL {
+		type Vec struct {
+			Symbol byte
+			Vector [InputSize]float32
+		}
+		type CC struct {
+			Mixer   Mix
+			Vectors [1024]Vec
+		}
+		var m [33]CC
+		for i := range m {
+			if *FlagMixer == "filtered" {
+				m[i].Mixer = NewFiltered()
+			} else {
+				m[i].Mixer = NewMixer()
+			}
+		}
+		for i := range m {
+			symbol := byte(0)
+			m[i].Mixer.Add(symbol)
+			for j := range m[i].Vectors {
+				m[i].Vectors[j].Vector = m[i].Mixer.Mix()
+				m[i].Vectors[j].Symbol = symbol
+				symbol = byte(rng.Intn(s))
+				m[i].Mixer.Add(symbol)
+			}
+		}
+		type Vector struct {
+			Index  int
+			Mixer  Mix
+			Vector [InputSize]float32
+			Rank   float64
+		}
+		for _, v := range in {
+			cp := [33]Vector{}
+			for j := range m {
+				cp[j].Index = j
+				cp[j].Mixer = m[j].Mixer.Copy()
+				cp[j].Mixer.Add(v)
+				cp[j].Vector = cp[j].Mixer.Mix()
+			}
+			graph := pagerank.NewGraph()
+			for j := 0; j < len(cp); j++ {
+				for k := 0; k < len(cp); k++ {
+					graph.Link(uint32(j), uint32(k), float64(CS(cp[j].Vector[:], cp[k].Vector[:])))
+				}
+			}
+			graph.Rank(1.0, 1e-6, func(node uint32, rank float64) {
+				cp[node].Rank = rank
+			})
+			sort.Slice(cp[:], func(i, j int) bool {
+				return cp[i].Rank > cp[j].Rank
+			})
+			max, index := float32(0.0), 0
+			for i := range m[cp[0].Index].Vectors {
+				cs := CS(m[cp[0].Index].Vectors[i].Vector[:], cp[0].Vector[:])
+				if cs > max {
+					max, index = cs, i
+				}
+			}
+			m[cp[0].Index].Mixer = cp[0].Mixer
+			fmt.Printf("%c", isymbols[int(m[cp[0].Index].Vectors[index].Symbol)])
+		}
+	}
 
 	if *FlagVDB {
 		var m Mix
